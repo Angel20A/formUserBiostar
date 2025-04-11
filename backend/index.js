bde0cd178987efa2069aaefb641a99c0fc3ed964
Angel <angelaragonf@gmail.com>
2025-04-11 15:59:10 -0600

    creacion de endpoint /login y /createUser, y consumo de login

--- backend/index.js
+++ backend/index.js
@@ -1,71 +1,77 @@
 import express from "express";
 import cors from "cors";
-import {config}from "./config.js";
 import fs from "fs";
 import https from "https";
 import fetch from "node-fetch";
 import axios from "axios";
 
-/*const getAPI = async() => {
-    const response = await fetch("https://127.0.0.1:8443/api/login",{
-        method: "GET",
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-            'accept': 'application/json',
-        },
-        body: {
-            "User": {
-                "login_id": "admin",
-                "password": "SDGsns2025"
-            }
-        }
-    }
-    );
-    const data = await response.json();
-    console.log(data);
-}
-getAPI();*/
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const instancia = axios.create({
+    baseURL: "https://127.0.0.1:8443/api",
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'accept': 'application/json',
+    },
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false // Desactiva la verificación del certificado SSL
+    })
+});
+
 app.post("/login", async (req, res) => {
     try{
-        const result = await axios.post("https://127.0.0.1:8443/api/login/", {
-            "User": {
-                "login_id": req.body.User.login_id,
-                "password": req.body.User.password
+        const result = await instancia.post("/login", req.body, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
             }
-        },{
-            headers:{
+        });
+        res.setHeader("bs-session-id", result.headers["bs-session-id"]);
+        const data = [result.data, result.headers["bs-session-id"]];
+        //res.send(result.data);
+        res.send(data);
+        
+        console.log(result.data);
+    }catch(error){
+        const message = error.message;
+        const status = error.status;
+        const code = error.code;
+        const errors = {message, status, code};
+        res.send(errors);
+
+        //res.status().json(error.message);
+        console.log(error.message);
+    }
+});
+
+app.post("/createUser", async (req, res) => {
+    try{
+        const result = await instancia.post("/users", req.body, {
+            headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json',
                 'accept': 'application/json',
-            },
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false // Desactiva la verificación del certificado SSL
-            })
+                'bs-session-id': req.headers["bs-session-id"]
+            }  
         });
-        res.json(result);
+        res.send(result);
         console.log(result);
     }catch(error){
-        res.status(error.response.status).json(error.message);
-        console.log(error);
+        const message = error.message;
+        const status = error.status;
+        const code = error.code;
+        const errors = {message, status, code};
+        res.send(error);
+
+        //res.send(error.response);
+        console.log(error.message);
     }
-})
+});
+
 app.listen(4000, () => {
     console.log("Aplicacion corriendo en el puerto 4000");
 });
-
-/*https.createServer({
-    cert: fs.readFileSync("./cert/serverBiostar.cer"),
-    key: fs.readFileSync("./cert/serverBiostar.key")
-}, app).listen(4000, function(){
-    console.log("Servidor HTTPS corriendo en el puerto 4000");
-});*/
-/*app.use(cors());
-app.use(express.json());
-app.listen(4000, () => {
-    console.log("Aplicacion corriendo en el puerto 4000");
-});*/
--- formLogin.js
+++ formLogin.js
@@ -1,16 +1,9 @@
-//const URLAPI = 'https://127.0.0.1:8443';
-const URLAPI = 'https://192.168.1.8:8443/api';
+//const URLAPI = 'https://127.0.0.1:8443/api';
+const URLAPI = 'http://localhost:4000';
+//const URLAPI = 'https://192.168.1.8:8443/api';
 const instancia = axios.create({
     baseURL: URLAPI,
-    /*headers:{
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'accept': 'application/json',
-    }*/
 });
-
-/*const res = instancia.get("/api/migration");
-console.log(res);*/
 
 async function login(){
     const login_id = document.getElementById("login_id").value;
@@ -19,24 +12,21 @@ async function login(){
     try{
         console.log(login_id);
         console.log(password);
-        const response = await instancia.post("/login/", {
+        const response = await instancia.post("/login", {
             "User":{
                 "login_id": login_id,
                 "password": password
             }
-        },{
-            headers:{            
-                'Access-Control-Allow-Origin': URLAPI,
-                'Content-Type': 'application/json',
-                'accept': 'application/json'
-            }
-        } 
-        );
-        console.log(response);
+        });
+        console.log(response.data[0]); //Imprime la data del usuario
+        console.log(response.data[1]); //Imprime el sessionId
+
         if(response.status === 200){
             alert("Login exitoso");
+            sessionStorage.setItem("sessionId", response.data[1]);
+            sessionStorage.setItem("user", JSON.stringify(response.data[0]));
             // Redirigir a la página de inicio
-            window.location.href = "index.html";
+            window.location.href = "formUser.html";
         }
 
     }catch(error){
